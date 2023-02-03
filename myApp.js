0a556f4a0ce9966e9863d0f5f78b2d1bddceb836
Carlos <rvjosecarlos@gmail.com>
2023-02-02 22:47:21 -0600

    Commit 1: Tema 1, correcciones en variables de entorno

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 
 const MONGO_URI = process.env['MONGO_URI'];
+console.log(MONGO_URI);
 
 mongoose.connect( MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
